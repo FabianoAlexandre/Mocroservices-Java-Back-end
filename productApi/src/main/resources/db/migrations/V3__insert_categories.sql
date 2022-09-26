0d1d3f591fce8f60cf57455edc07ef79c623cd88
Fabiano Alexandre <fabiano.alexandred@gmail.com>
2022-09-26 11:43:44 -0300

    Created Resources Model, Repository, DTO and migrations

---
+++ productApi/src/main/resources/db/migrations/V3__insert_categories.sql
@@ -0,0 +1,3 @@
+INSERT INTO products.category(id, nome) values(1, 'Eletrônicos');
+INSERT INTO products.category(id, nome) values(2, 'Móveis');
+INSERT INTO products.category(id, nome) values(3, 'Brinquedos');
